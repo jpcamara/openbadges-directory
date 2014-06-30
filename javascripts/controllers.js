c6525e35ee741e057cfecda24d3aac86cc6d986b
jpcamara <johnpcamara@gmail.com>
2014-06-30 17:51:55 -0400

    register

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -20,11 +20,11 @@ appPageControllers.controller('RegisterController', ['$scope', '$http', '$window
       console && console.log && console.log(data, status);
       alert('There was an error during your registration. Please try again.');
     });
-
-    $scope.$watch('name', function () {
-      console.log($scope.name);
-    });
   };
+
+  $scope.$watch('name', function () {
+    console.log($scope.name);
+  });
 //    $http.get(endpoint + '/search?page=' + page + (query.length ? '&' + query.join('&') : '')).success(function (response) {
 //      if (!response.data.length) { done = true; }
 //      if ($scope.badges && $scope.badges.length) {
